db9903f88ad436a1e78cfad40e1ff89e4dc2fb05
George Vine <vinegh4@gmail.com>
2022-09-10 23:05:05 -0700

    began laying out hitrecord and hit function

--- src/raytracing.rs
+++ src/raytracing.rs
@@ -5,9 +5,18 @@ pub struct Ray {
     pub direction: Vector3
 }
 
-
 pub fn cast_ray (ray: Ray, dist: f64) -> Vector3 {
         ray.origin + (ray.direction  * dist)
+}
+
+pub struct HitRecord {
+    hitpoint: Vector3,
+    normal: Vector3,
+    t: f64
+}
+
+pub trait Hittable {
+    fn hit(ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool;
 }
 
 
